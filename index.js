83f69a390f4a91d4cdcdb293e3c8a219315b8dcb
mikehov <mikehovje@gmail.com>
2020-05-08 15:54:08 +0200

    Backend express files

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/about', (req, res) => res.send('This is the about page.'))
+app.get('/contact', (req, res) => res.send('This is the contact page.'))
+app.get('*', (req, res) => res.send('404 Page not loading...'))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
