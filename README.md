5a97bf9853a557a30a808ad6f97b3faa6160b4d5
mikehov <mikehovje@gmail.com>
2020-06-02 19:21:36 +0200

    Backend-Like-system

--- README.md
+++ README.md
@@ -15,3 +15,96 @@ This dating-app is about the feature "swiping" and "notifications". Its a casual
 To find the documentation, check out my wiki page of my repository. 
 https://github.com/mikehov/Dating-app/wiki
 
+## Sources
+AWS Simplified. (2020, February 10). SQL vs NoSQL Explained [Video file]. YouTube. Retrieved from https://www.youtube.com/watch?v=ruz-vK8IesE&t=310s
+
+Bitovi. (n.d.). Tinder Carousel | intermediate recipes | Guides | CanJS — Build CRUD apps in fewer lines of code. Retrieved June 2, 2020, from https://canjs.com/doc/guides/recipes/tinder-carousel.html
+
+Box Shadow CSS Generator | CSSmatic. (n.d.). Retrieved May 10, 2020, from https://www.cssmatic.com/box-shadow 
+
+Claire is seasoned technical writer, editor, and HTML enthusiast. She writes for HTML.com and runs a content agency, Red Robot Media. (n.d.). Attribute for METHOD = GET | POST. Retrieved May 26, 2020, from https://html.com/attributes/form-method/
+
+de Vries, D. (n.d.-a). lecture-3. Retrieved June 2, 2020, from https://docs.google.com/presentation/d/1PJTtYNOFw8n9Zr54kBiabNpb777QDMNJIFt0bKVR__Q/edit#slide=id.g4e3b0a74b9_1_416
+
+freeCodeCamp.org. (2018, October 25). Let’s Build a Tinder-Like Swipe Carousel [Video file]. In YouTube. Retrieved from https://www.youtube.com/watch?v=Iz6W4HizXq8
+```
+Code
+```
+
+de Vries, D. (n.d.). lecture-4. Retrieved June 2, 2020, from https://docs.google.com/presentation/d/1J0SVcx7rMnFp37JqsQMHQq92EfBRUFdgSAj5i9wQKjg/edit#slide=id.g85a98fc989_0_63
+```
+Code
+```
+
+MongoDB. (n.d.-b). Managed MongoDB Hosting | Database-as-a-Service. Retrieved June 2, 2020, from https://www.mongodb.com/cloud/atlas
+
+MongoDB. (n.d.-b). Connect to MongoDB. Retrieved June 2, 2020, from https://docs.mongodb.com/guides/server/drivers/
+
+Sarzyński, Ł. (n.d.). fill HTML forms without JavaScript. Retrieved May 26, 2020, from https://dev.to/lukaszsarzynski/fill-html-forms-without-javascript-4d24
+
+SVG drop shadow using css3. (2011, May 22). Retrieved May 10, 2020, from https://stackoverflow.com/questions/6088409/svg-drop-shadow-using-css3
+
+Code from the student Rowin Ruizendaal:
+```
+app.post("/matches", function (req, res) {
+  db.collection('Users').find() 
+    .toArray(function (err, data) {
+      if (err) console.log(err);
+      // console.log(data);
+      let allIds = [];
+      for (let i of data) {
+        allIds.push(i._id);
+      }
+      console.log(allIds[index]);
+      index++;
+      console.log('Dit is de index' + index)
+
+      if (index == allIds.length) {
+        index = 0;
+      }
+
+      console.log(data[index]);
+      console.log(req.body)
+
+      // Random distance counter
+      let randomDistance = Math.floor(Math.random() * 100);
+      data[index].randomDistance = randomDistance;
+      console.log(data);
+      res.render('swipe.ejs', {
+        data: data[index]
+      }); 
+    });
+});
+
+function add(req, res) {
+  data.push({
+    fullname: req.body.fullname,
+    age: req.body.age,
+    description: req.body.description,
+  });
+
+  res.redirect('/list');
+}
+
+mongodb.MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  },
+  function (err, client) {
+    if (err) throw err;
+    db = client.db(process.env.DB_NAME);
+    console.log('Connected to database')
+  });
+```
+
+#### Pictures
+Christopher, T. (2017, May 21). Toby Christopher [Photo]. Retrieved from https://unsplash.com/photos/P-CeaGknSJA
+
+Hefitiba, T. (2018, February 5). Toa Heftiba [Photo]. Retrieved from https://unsplash.com/photos/O3ymvT7Wf9U
+
+Hiles, S. (2017, December 26). Sonnie Hiles [Photo]. Retrieved from https://unsplash.com/photos/gG70fyu3qsg
+
+Jensen, D. (2017, August 15). Daniel Jensen [Photo]. Retrieved from https://unsplash.com/photos/tQpypKA92k8
+
+Kobalasingam, H. (2018, November 8). Harishan Kobalasingam [Photo]. Retrieved from https://unsplash.com/photos/8PMvB4VyVXA
+
+
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+// All the packages
 const express = require('express');
-const slug = require('slug');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
-// Reads dotenv file
+// const slug = require('slug');
 require('dotenv').config();
 
 // Connect to the MongoDB Atlas 
@@ -12,7 +12,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${p
 mongodb.MongoClient.connect(uri, {
     useUnifiedTopology: true,
   },
-  (err, client) => {
+  function (err, client) {
     if (err) throw err;
     db = client.db(process.env.DB_NAME);
     console.log('Connected to database')
@@ -22,7 +22,7 @@ const app = express();
 const port = 5000;
 
 
-// folder page
+// Folder page
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -31,26 +31,14 @@ app.use(express.static(`${__dirname}/views`));
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// links to the .ejs files
-app.get("/index", (req, res) => {
-  db.collection('Users')
-    .find()
-    .toArray((err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(data);
-        res.render('swipe.ejs', {
-          data: data,
-        });
-      }
-    });
+// Links to the .ejs files
+app.get("/index", function (req, res) {
+  res.render("index.ejs");
 });
-
-app.get('/list', (req, res) => {
+app.get('/list', function (req, res) {
   db.collection('Users')
     .find()
-    .toArray((err, data) => {
+    .toArray(function (err, data) {
       if (err) console.log(err);
       console.log(data);
       res.render('list.ejs', {
@@ -58,26 +46,82 @@ app.get('/list', (req, res) => {
       });
     });
 });
-app.get("/add", (req, res) => {
-  res.render("add.ejs", {
-    data
-  });
+app.get("/add", function (req, res) {
+  res.render("add.ejs");
+  console.log('User has been added to the list')
 });
-
-app.get("/match", (req, res) => {
+app.get("/match", function (req, res) {
   res.render("match.ejs");
 });
-app.post("/test", (req, res) => {
+
+// Links to the database collection
+
+// Logs if someone liked or disliked
+let index = 0;
+// Logs id's
+app.post("/matches", function (req, res) {
+  db.collection('Users').find() 
+    .toArray(function (err, data) {
+      if (err) console.log(err);
+      // console.log(data);
+      let allIds = [];
+      for (let i of data) {
+        allIds.push(i._id);
+      }
+      console.log(allIds[index]);
+      index++;
+      console.log('Dit is de index' + index)
+
+      if (index == allIds.length) {
+        index = 0;
+      }
+
+      console.log(data[index]);
+      console.log(req.body)
+
+      // Random distance counter
+      let randomDistance = Math.floor(Math.random() * 100);
+      // console.log(typeof data);
+      data[index].randomDistance = randomDistance;
+      // console.log('The distance is ' + data.randomDistance + ' km');
+      console.log(data);
+      res.render('swipe.ejs', {
+        data: data[index]
+      }); 
+    });
+});
+
+app.post("/user", function (req, res) {
   db.collection('Users').insertOne({
-    'username': req.body.username,
+    'firstname': req.body.firstname,
+    'lastname': req.body.lastname,
+    'email': req.body.email,
     'gender': req.body.gender,
     'age': req.body.age,
+    'lookingfor': req.body.lookingfor,
+    'city': req.body.city,
+    'characteristics': req.body.characteristics,
   });
   res.redirect("/list");
 });
 
 
-// eslint-disable-next-line require-jsdoc
+// app.post("/matches", function (req, res) {
+//   db.collection('Users').insertOne({
+//     'firstname': req.body.firstname,
+//     'lastname': req.body.lastname,
+//     'email': req.body.email,
+//     'gender': req.body.gender,
+//     'age': req.body.age,
+//     'lookingfor': req.body.lookingfor,
+//     'city': req.body.city,
+//     'characteristics': req.body.characteristics,
+//   });
+//   console.log(req.body.liking - btn);
+//   res.redirect("/list");
+// });
+
+// Eslint disable next line require jsdoc
 function add(req, res) {
   data.push({
     fullname: req.body.fullname,
@@ -88,54 +132,6 @@ function add(req, res) {
   res.redirect('/list');
 }
 
-// .use('/', movies)
-// .get('/add', form)
-// .get('/:id', movie)
-
-// function form(req, res) {
-//   res.render('add.ejs')
-// }
-
-// eslint-disable-next-line no-undef
-// app.get("/", index);
-// eslint-disable-next-line no-use-before-define
-// app.get("/profile/:name", files);
-
-// NOT NEEDED AT THE MOMENT
-// function files(req, res) {
-//   if (req.params === "mp3") {
-//     res.send("Send mp3 file!");
-//   } else if (req.params === "image") {
-//     res.send("Send image file!");
-//   } else if (req.params === "pdf") {
-//     res.send("Send pdf file!");
-//   } else {
-//     res.send("Nothing");
-//   }
-// }
-
-// function files(req, res) {
-//     console.log('Hey');
-//     escape.send('Hello files');
-// }
-
-// app.get('/home', (req, res) => res.sendFile(__dirname + '/index.html'))
-// app.use(express.static(__dirname + ''));
-
-// app.get('/about', (req, res) => res.send('This is the about page.'))
-// app.get('/contact', (req, res) => res.send('This is the contact page.'))
-// app.get('/test', (req, res) => {
-//     res.send('This is the test page with console.');
-//     console.log(req.headers.host + req.url);
-// })
-
-// app.get('/mp3', (req, res) => {
-//     res.sendFile(__dirname + '/0001.mp3');
-//     console.log(req.headers.host + req.url);
-// })
-// app.get('*', (req, res) => res.send('404 Page not loading...'))
-
-// eslint-disable-next-line no-console
-app.listen(port, () =>
+app.listen(port, function() {
   console.log(`Example app listening at http://localhost:${port}`)
-);
+});
