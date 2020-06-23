704256c3f0001b9eded0d56ea92f1bb42b54e4c3
mikehov <mikehovje@gmail.com>
2020-06-23 13:58:11 +0200

    Frontend a2

--- views/js/script.js
+++ views/js/script.js
@@ -9,3 +9,18 @@ console.log(randomCounter());
 
 // eslint-disable-next-line max-len
 document.querySelector('.person-information p:nth-child(3)').innerHTML = randomCounter() + 'km away from you';
+
+// const listCharacteristics = ['happy', 'cool', 'kind', 'friendly', 'clever', 'gentle', 'polite', 'original'];
+
+// // eslint-disable-next-line require-jsdoc
+// function calcRandomCharacteristics() {
+//   const randomCharacteristic = listCharacteristics[Math.floor(Math.random() * listCharacteristics.length)];
+//   return randomCharacteristic;
+// }
+
+// // eslint-disable-next-line max-len
+// document.querySelector('.person-information div p:nth-child(1)').innerHTML = calcRandomCharacteristics();
+// document.querySelector('.person-information div p:nth-child(2)').innerHTML = calcRandomCharacteristics();
+// document.querySelector('.person-information div p:nth-child(3)').innerHTML = calcRandomCharacteristics();
+
+// console.log(calcRandomCharacteristics());
