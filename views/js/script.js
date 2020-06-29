b2a8862126057ae87de418928ff8871424d7784a
mikehov <mikehovje@gmail.com>
2020-06-29 15:55:47 +0200

    a2

--- views/js/script.js
+++ views/js/script.js
@@ -1,18 +1,20 @@
 console.log('Javascript works');
 
+// selecting the html elements
 const data = document.querySelectorAll('.listsearch');
-// eslint-disable-next-line no-unused-vars
 const characteristics = document.querySelectorAll('.characteristics');
 
+// selecting the inputfield, checks if the value changes of the input field
 const inputField = document.querySelector('input');
 inputField.addEventListener('input', function(event) {
-  const karel = inputField.value;
+  const letters = inputField.value;
 
+  // loops through all users
   for (i = 0; i < data.length; i++) {
     const allItems = data[i];
     const allCharacteristics = allItems.dataset.characteristics;
-    // console.log(allItems.dataset.characteristics.split(','));
-    if (allCharacteristics.indexOf(karel) > -1) {
+    // if the letters are matching the characteristics, keep the item visible, otherwise make it invisible
+    if (allCharacteristics.indexOf(letters) > -1) {
       allItems.classList.remove('invisible');
     } else {
       allItems.classList.add('invisible');
